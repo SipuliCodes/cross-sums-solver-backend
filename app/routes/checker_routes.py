4ef8b1f7c4635320202596cd01a69f00d62fbf2a
SipuliCodes <vepsalainensisu@gmail.com>
2025-01-29 12:11:12 +0200

    Update checker route
    Added validation for the incoming data that checks that the sizes of targets and grid in correct

--- app/routes/checker_routes.py
+++ app/routes/checker_routes.py
@@ -8,9 +8,28 @@ bp = Blueprint('checker_routes', __name__)
 def check_answer():
     data = request.json
     if "row_targets" not in data or "col_targets" not in data or "row_weights" not in data or "col_weights" not in data:
-        abort(400)
+        return "Some attributes missing", 400
+
+    
+    if len(data["row_targets"]) != len(data["col_targets"]):
+         return "Targets not same length", 400
+    
+    grid_size = len(data["col_targets"])
+
+    if not correct_grid_size(grid_size, data["row_weights"], data["col_weights"]):
+        return "Grid size error", 400
+
         
     return calculate_cross_sums(data["row_targets"], data["col_targets"], data["row_weights"], data["col_weights"])
+
+def correct_grid_size(grid_size, row_weights, col_weights):
+    if len(row_weights) != grid_size or len(col_weights) != grid_size:
+        return False
+    
+    for i in range(grid_size):
+        if len(row_weights[i]) != grid_size or len(col_weights[i]) != grid_size:
+            return False
+    return True
     
     
 
